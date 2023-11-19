f8dfe58dc648d358071e2553edf820a87343e180
echizenyayota <virtualmisso30@gmail.com>
2023-11-19 21:52:57 +0900

    add a select tag with three levels

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import axios from "axios";
+import { useState } from "react";
 
-const App = async () => {
+const App = () => {
 
-  const options = {
-    method: 'GET',
-    url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
-    params: {
-      level: '3',
-      area: 'sat'
-    },
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST
+  const [chosenLevel, setChosenLevel] = useState(null);
+
+  const getRandomWords = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+      params: {
+        level: '3',
+        area: 'sat'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST
+      }
+    };
+  
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
     }
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
   }
+
+  console.log(chosenLevel);
+  
 
   return (
     <div className="App">
-
+      <select
+        name="levels"
+        id="levels"
+        value={""}
+        onChange={(e) => setChosenLevel(e.target.value)}
+      >
+        <option value="1">Level 1</option>
+        <option value="2">Level 2</option>
+        <option value="3">Level 3</option>
+      </select>
     </div>
   );
 }

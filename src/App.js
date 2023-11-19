0f49f21e3c750f52702db734751c9153b0478038
echizenyayota <virtualmisso30@gmail.com>
2023-11-19 21:21:10 +0900

    get the word-data from Word Quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-const App = () => {
+import axios from "axios";
+
+const App = async () => {
+
+  const options = {
+    method: 'GET',
+    url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+    params: {
+      level: '3',
+      area: 'sat'
+    },
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
-    <div className="app">
-      
+    <div className="App">
+
     </div>
   );
 }
-
 export default App;

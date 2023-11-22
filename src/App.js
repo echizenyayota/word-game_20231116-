a72acfc5d7bf109ff41d9afadad1972c4e8873dc
echizenyayota <virtualmisso30@gmail.com>
2023-11-22 22:08:02 +0900

    add the levels up to 10

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 const App = () => {
 
-  const [chosenLevel, setChosenLevel] = useState("2");
+  const [chosenLevel, setChosenLevel] = useState(null);
   const [words, setWords] = useState(null);
   const [correctAnswers, setCorrectAnswers] = useState([]);
   const [clicked, setClicked] = useState([]);
@@ -64,6 +64,13 @@ const App = () => {
           <option value="1">Level 1</option>
           <option value="2">Level 2</option>
           <option value="3">Level 3</option>
+          <option value="4">Level 4</option>
+          <option value="5">Level 5</option>
+          <option value="6">Level 6</option>
+          <option value="7">Level 7</option>
+          <option value="8">Level 8</option>
+          <option value="9">Level 9</option>
+          <option value="10">Level 10</option>
         </select>
       </div>}
 

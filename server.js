eca40e60b6775557223e18e88e2682f67b70ad67
echizenyayota <virtualmisso30@gmail.com>
2023-11-23 22:03:29 +0900

    set the route to contact with the API

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ const axios = require("axios");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("Hi!");
+});
+
+app.get("/results", (req, res) => {
+  res.json("results!");
+});
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
